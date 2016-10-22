fbe5e4557282d6a11fa46e3e92ba66c0aae6244e
Rajat Jain <rajat@hackerrank.com>
2016-10-22 19:25:26 +0530

    added credentials

---
+++ config/credentials.php
@@ -0,0 +1,8 @@
+<?php
+  return array(
+    'consumer_key' => getenv('CONSUMER_KEY'),
+    'oauth_access_token' => getenv('OAUTH_TOKEN'),
+    'consumer_secret' => getenv('CONSUMER_SECRET'),
+    'oauth_access_token_secret' => getenv('OAUTH_SECRET')
+  );
+?>
